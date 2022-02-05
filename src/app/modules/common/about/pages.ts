bdc446a086c83015980e1f9491efde2a65730481
AndrewJBateman <gomezbateman@yahoo.com>
2022-02-05 19:23:05 +0100

    feat: updated html coontent

--- src/app/modules/common/about/pages.ts
+++ src/app/modules/common/about/pages.ts
@@ -4,20 +4,22 @@ export const PAGES: Page[] = [
   {
     title: 'App',
     icon: 'devices',
-    content: 'This app was built in Angular v10 with Angular Material Design components/icons. There is a top menu for desktop/tablet screens and a burger icon-activated side menu for phone screens. The RxJS reactive programming library is used to handle datastreams and propagation of change using observables',
+    content:
+      'This app was built in Angular v13 with Angular Material Design components/icons. There is a top menu for desktop/tablet screens and a burger icon-activated side menu for phone screens. The RxJS reactive programming library is used to handle datastreams and propagation of change using observables',
     website1: 'Angular',
     link1: 'https://angular.io/',
     website2: 'Angular Material',
-    link2: 'https://material.angular.io/'
+    link2: 'https://material.angular.io/',
   },
   {
     title: 'Home',
     icon: 'home',
-    content: 'Global data for Covid-19 cases is fetched from a coronatracker API, set in local storage then displayed on a Mat-card. The Apapi API is used to determine the user country and Covid-19 cases are also shown from that country. Global Covid-19 cases are shown on a chart using the angular-google-charts module',
+    content:
+      'Global data for Covid-19 cases is fetched from a coronatracker API, set in local storage then displayed on a Mat-card. The Apapi API is used to determine the user country and Covid-19 cases are also shown from that country. Global Covid-19 cases are shown on a chart using the angular-google-charts module',
     website1: 'coronatracker API',
     link1: 'https://api.coronatracker.com',
     website2: 'Apapi API',
-    link2: 'https://ipapi.co/'
+    link2: 'https://ipapi.co/',
   },
   {
     title: 'Country List',
@@ -26,7 +28,7 @@ export const PAGES: Page[] = [
     website1: 'Countryflags API',
     link1: 'https://www.countryflags.io',
     website2: 'Angular number pipe',
-    link2: 'https://angular.io/api/common/DecimalPipe#description'
+    link2: 'https://angular.io/api/common/DecimalPipe#description',
   },
   {
     title: 'News',
@@ -35,13 +37,13 @@ export const PAGES: Page[] = [
     website1: 'day.js npm module',
     link1: 'https://www.npmjs.com/package/dayjs',
     website2: 'Angular nav extras',
-    link2: 'https://angular.io/api/router/NavigationExtras'
+    link2: 'https://angular.io/api/router/NavigationExtras',
   },
   {
     title: 'Charts',
     icon: 'analytics',
     content: `Global Covid case data is displayed by country using the Angular-Google-Charts npm module with thresholds set to limit the data to make the charts more readable. A radio button group allows the user to choose which class of data is displayed; confirmed, recovered or dead`,
     website1: 'Angular Google Charts',
-    link1: 'https://www.npmjs.com/package/angular-google-charts'
-  }
+    link1: 'https://www.npmjs.com/package/angular-google-charts',
+  },
 ];
